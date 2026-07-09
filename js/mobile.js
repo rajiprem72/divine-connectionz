0fa61fcec6e45a1dd2d2f9dea9533955162fc73e
Premanandhan Narayanan <premanandhan@gmail.com>
2026-07-09 15:17:36 +0530

    Fix menu element ID selector in mobile.js

--- js/mobile.js
+++ js/mobile.js
@@ -6,7 +6,7 @@
 document.addEventListener("DOMContentLoaded", function () {
 
     const button = document.querySelector(".menu-toggle");
-    const menu = document.getElementById(".menu-wrapper");
+    const menu = document.getElementById("menu-wrapper");
 
     button.addEventListener("click", function () {
 
