b87511793c828d0bf9cd981347f9f148ca5b1de3
Premanandhan Narayanan <premanandhan@gmail.com>
2026-07-11 16:56:27 +0530

    Update search-youtube.js

--- js/search-youtube.js
+++ js/search-youtube.js
@@ -1,47 +1,61 @@
      const API_KEY = 'AIzaSyDAVB7a599t2To9oz8oP__1_bhNb4YOoRg';
-        const channelId = 'UCUdlewvf5aiwwG5AQ2v3Y0Q';
-        function searchYoutube(){
-            const searchValue = document.getElementById("search").value;
-            const searchUrl = `https://www.googleapis.com/youtube/v3/search?
-part=snippet&q=${searchValue}&type=video&channelId=${channelId}&maxResults=5&key=
-${API_KEY}`;
-          fetch(searchUrl)
-    .then(response => response.json())
-    .then(data => {
+     const channelId = 'UCUdlewvf5aiwwG5AQ2v3Y0Q';
 
-        console.log(data);
+function searchYoutube() {
 
-        if (data.error) {
-            alert(
-                data.error.message +
-                "\n\nReason: " +
-                data.error.errors[0].reason
-            );
-            return;
-        }
+    const searchValue = document.getElementById("search").value;
 
-        const resultsBody = document.getElementById('results-body');
+    const searchUrl = `https://www.googleapis.com/youtube/v3/search?part=snippet&q=${searchValue}&type=video&channelId=${channelId}&maxResults=5&key=${API_KEY}`;
 
-        resultsBody.innerHTML = "";
+    fetch(searchUrl)
 
-        data.items.forEach(function(item) {
+        .then(response => response.json())
 
-            const videoId = item.id.videoId;
-            const videoTitle = item.snippet.title;
+        .then(data => {
 
-            const videoLink =
-                `https://www.youtube.com/watch?v=${videoId}`;
+            console.log(data);
 
-            resultsBody.innerHTML += `
-                <tr>
-                    <td>${videoTitle}</td>
-                    <td>
-                        <a href="${videoLink}" target="_blank">
-                            ${videoLink}
-                        </a>
-                    </td>
-                </tr>
-            `;
+            if (data.error) {
+
+                alert(
+                    data.error.message +
+                    "\n\nReason: " +
+                    data.error.errors[0].reason
+                );
+
+                return;
+            }
+
+            const resultsBody = document.getElementById('results-body');
+
+            resultsBody.innerHTML = "";
+
+            data.items.forEach(function(item) {
+
+                const videoId = item.id.videoId;
+
+                const videoTitle = item.snippet.title;
+
+                const videoLink = `https://www.youtube.com/watch?v=${videoId}`;
+
+                resultsBody.innerHTML += `
+                    <tr>
+                        <td>${videoTitle}</td>
+                        <td>
+                            <a href="${videoLink}" target="_blank">
+                                ${videoLink}
+                            </a>
+                        </td>
+                    </tr>
+                `;
+            });
+
+        })
+
+        .catch(error => {
+
+            console.error(error);
+
         });
 
-    });
+}
